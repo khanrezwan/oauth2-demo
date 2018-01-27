fca73364b5ecdad632860317724d356118a541cc
Rezwan <rezwanislam@gmail.com>
2018-01-28 02:50:09 +0600

    added urls for tuts

--- src/main/java/com/rezwanislam/oauth2demo/Oauth2DemoApplication.java
+++ src/main/java/com/rezwanislam/oauth2demo/Oauth2DemoApplication.java
@@ -12,3 +12,9 @@ public class Oauth2DemoApplication {
 		SpringApplication.run(Oauth2DemoApplication.class, args);
 	}
 }
+
+//todo following these tuts
+// https://gigsterous.github.io/engineering/2017/03/01/spring-boot-4.html
+// https://github.com/dsyer/sparklr-boot
+// https://jugbd.org/2017/09/19/implementing-oauth2-spring-boot-spring-security/
+// https://docs.spring.io/spring-security/oauth/apidocs/org/springframework/security/oauth2/provider/package-summary.html
